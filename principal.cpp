dac07b378ed55bb2b174f218edd4f0d12bcef6d9
Gabriel Vasconcelos <93106982+gabrielvascf@users.noreply.github.com>
2023-11-07 22:25:10 -0300

    Atributos

--- principal.cpp
+++ principal.cpp
@@ -1,3 +1,54 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Atividade {
+private:
+    vector<string, string> res;
+
+public:
+    string data;
+    string descricao;
+};
+
+struct Paciente {
+
+
+public:
+    Paciente(string nome, string nasc, string cidade, string estado, string cfisicas, string cmentais) {
+        nome = nome;
+        data_nascimento = nasc;
+        cidade = cidade;
+        estado = estado;
+        cfisicas = cfisicas;
+        cmentais = cmentais;
+
+
+    }
+
+    void setnome (){
+        cout << "Nome: "<<endl;
+        cin>> nome;
+    }
+     string getnome(){
+       return nome;
+     }
+
+private:
+        string nome;
+        string data_nascimento;
+        string cidade;
+        string estado;
+        string cfisicas;
+        string cmentais;
+      //  Atividade historico[30];
+
+};
+
+
 int main() {
+    Paciente p = Paciente("luis", "hoje", "aqui", "aqui tambem", "ok", "maluco");
+    cout << p.nome;
 
 }
